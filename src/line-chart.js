05fb5fb25219ed7e651a6c062c069f44a2482602
Anton Malmquist <anton@interaktionsbyran.se>
2018-08-17 12:46:24 +0200

    fix: Fixes syntactical error

--- src/line-chart.js
+++ src/line-chart.js
@@ -159,7 +159,7 @@ class LineChart extends AbstractChart {
   render() {
     const paddingTop = 16
     const paddingRight = 64
-    const { width, height, data, withShadow = true, withDots = true, style = {}, unit=''. precision=2 } = this.props
+    const { width, height, data, withShadow = true, withDots = true, style = {}, unit='', precision=2 } = this.props
     const { labels = [] } = data
     const { borderRadius = 0 } = style
     const config = {
